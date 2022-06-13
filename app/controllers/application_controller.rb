cc16ac3db3160ee6aefb7a12e648ca2a05a13fcb
lrjbrual <lrjbrual@live.com>
2022-06-13 16:44:40 +0200

    adding current company

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
  # Add this line to see the progress bar long enough
   # and remove it when it has the expected styles
-  # before_action -> { sleep 3 }
+  # before_action -> { sleep 5 }
   before_action :authenticate_user!, unless: :devise_controller?
 
   private 
